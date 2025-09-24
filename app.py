c2d95de647faec5f7b7c1fc005c3e86902f54dbb
TheOne101Alpha <josephachi49@gmail.com>
2025-09-24 12:37:36 -0400

    debut connexion bd

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
+import bd
 
-SITENOM = 'Services'
+
+
+SITENOM = 'Services-TP1'
 
 app = Flask(__name__)
 
 @app.route('/')
 def base():
     """Affiche la page d'aceuil du site"""
-    return render_template('base.jinja', titre=SITENOM)
+    with bd.creer_connexion() as connexion:
+        with connexion.get_curseur() as curseur:
+            curseur.execute("SELECT cat.nom_categorie, ser.titre, ser.description, ser.cout FROM services ser INNER JOIN categories cat ON cat.id_categorie = ser.id_service ORDER BY ser.date_creation LIMIT 5")
+            retour = curseur.fetchall()
+
+    return render_template('acceuil.jinja', titre=SITENOM, langue = "fr_CA", list=retour)
 
 
 
